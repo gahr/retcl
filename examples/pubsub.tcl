f90be35ba30af58e4d599614c963093aa5990dad
gahr <gahr>
2017-05-09 14:41:35 +0000

    Tidy pubsub.tcl example

--- examples/pubsub.tcl
+++ examples/pubsub.tcl
@@ -5,26 +5,28 @@
 tcl::tm::path add ..
 package require retcl
 
-proc mycallback {registrationTime type pattern channel message} {
+set forever 0
+
+proc mycallback {obj registrationTime type pattern channel message} {
     set elapsed [expr {[clock seconds] - $registrationTime}]
     puts "After $elapsed seconds I got a message of type $type"
     puts "on my registration channel $pattern."
     puts "The actual channel was $channel. The message is $message."
+
+    if {$type eq {pmessage}} {
+        $obj destroy
+        set ::forever 1
+    }
 }
 
-retcl create r
-r callback chan* [list mycallback [clock seconds]]
-r PSUBSCRIBE chan*
+set r [retcl new]
+$r callback chan* [list mycallback $r [clock seconds]]
+$r PSUBSCRIBE chan*
 
 after 3000 {
     retcl create r2
     r2 -sync PUBLISH chan1 Hello
-}
-
-after 3200 {
-    set forever 1
+    r2 destroy
 }
 
 vwait forever
-r destroy
-r2 destroy
