73ca4df1073bb23344d2fb195a7d4825dfc5ebd0
Pietro Cerutti <gahr@gahr.ch>
2017-05-12 14:29:02 +0000

    Add a simple pipeline test, gather common test code

--- test/all.tcl
+++ test/all.tcl
@@ -4,7 +4,14 @@ package require tcltest 2.3
 tcl::tm::path add [file join [file dirname [info script]] .. ]
 package require retcl
 
-tcltest::configure {*}$argv -singleproc 1 -testdir [file dir [info script]]
+tcltest::configure {*}$argv -singleproc 1 -testdir [file dir [info script]] \
+    -load {
+        proc runEvents {} {
+            set done 0
+            after 50 { set done 1 }
+            vwait done
+        }
+    }
 
 # Check that the server is up and running
 if {[catch {socket localhost 6379} fd]} {
